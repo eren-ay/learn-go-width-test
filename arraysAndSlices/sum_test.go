0e144fbeebaa87aac9aae512a19befa9f5a3f34a
eren <eren-ay35@hotmail.com>
2023-05-23 23:45:22 +0300

    some sum test added

--- arraysAndSlices/sum_test.go
+++ arraysAndSlices/sum_test.go
@@ -1,15 +1,42 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	numbers := []int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	firstNumberArray := []int{1, 5}
+	secondNumberArray := []int{6, 9}
+
+	got := sumAll(firstNumberArray, secondNumberArray)
+	want := []int{6, 15}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }

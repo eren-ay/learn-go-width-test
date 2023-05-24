4656cf9b166dfbc5b881002712e78b7db430b118
eren-ay <erenay494@gmail.com>
2023-05-24 21:18:37 +0300

    methods and interfaces added

--- structMethodsInterfaces/shapes.go
+++ structMethodsInterfaces/shapes.go
@@ -1,11 +1,47 @@
 package shape
 
-func Perimeter(widt float64, height float64) float64 {
-	perimeter := (widt + height) * 2
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// simple func
+func rectPerimeter(rectangle Rectangle) float64 {
+	perimeter := 2 * (rectangle.Width + rectangle.Height)
 	return perimeter
 }
 
-func Area(width float64, height float64) float64 {
-	area := width * height
+func rectArea(rectangle Rectangle) float64 {
+	area := rectangle.Width * rectangle.Height
 	return area
+}
+
+// this func calling like Circle.Area()= this func belongs to circle struct
+func (c Circle) Area() float64 {
+	area := math.Pi * c.Radius * c.Radius
+	return area
+}
+
+func (r Rectangle) Area() float64 {
+	area := r.Width * r.Height
+	return area
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- structMethodsInterfaces/shapes_test.go
+++ structMethodsInterfaces/shapes_test.go
@@ -3,7 +3,8 @@ package shape
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectPerimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,10 +13,29 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		want := 72.0
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+	t.Run("triangle", func(t *testing.T) {
+		triangle := Triangle{4.0, 3.0}
+		want := 6.0
+		checkArea(t, triangle, want)
+	})
 }
